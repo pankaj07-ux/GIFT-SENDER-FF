027305d544b32005d78ece7fc484d3b57c7dce83
Pankaj Sah <psah.pankajff@gmail.com>
2026-05-31 18:12:23 +0545

    Add files via upload

--- GetWallet_pb2.py
+++ GetWallet_pb2.py
@@ -32,7 +32,7 @@ _sym_db = _symbol_database.Default()
 DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0fGetWallet.proto\x12\x05proto\"l\n\rAccountWallet\x12\r\n\x05\x63oins\x18\x01 \x01(\r\x12\x0c\n\x04gems\x18\x02 \x01(\x05\x12\x10\n\x08gop_gems\x18\x03 \x01(\r\x12\x13\n\x0btotal_topup\x18\x04 \x01(\x04\x12\x17\n\x0flast_topup_time\x18\x05 \x01(\x03\";\n\x0e\x43SGetWalletReq\x12\x13\n\x0blogin_token\x18\x01 \x01(\t\x12\x14\n\x0ctopup_rebate\x18\x02 \x01(\x08\"^\n\x0e\x43SGetWalletRes\x12\x12\n\naccount_id\x18\x01 \x01(\x04\x12$\n\x06wallet\x18\x02 \x01(\x0b\x32\x14.proto.AccountWallet\x12\x12\n\npaid_level\x18\x03 \x01(\rb\x06proto3')
 
 # Created By pankaj-ux
-# Creator main channel @FFbotsALL
+# Discord : cyleader_
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -48,4 +48,4 @@ if not _descriptor._USE_C_DESCRIPTORS:
 # @@protoc_insertion_point(module_scope)
 
 # Created By pankaj-ux
-# Creator main channel @FFbotsALL
+# Discord : cyleader_
